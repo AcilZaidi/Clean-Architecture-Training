570d7ba3406bb5d0c8972616ad02377a407e1fb5
Acil Zaidi <92874240+AcilZaidi@users.noreply.github.com>
2024-03-11 17:03:40 +0100

    handler

--- PostLand.Application/Profiles/AutoMapperProfile.cs
+++ PostLand.Application/Profiles/AutoMapperProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Postland.Domain;
+using PostLand.Application.Features.Posts.Commands.CreatePost;
+using PostLand.Application.Features.Posts.Queries.GetPostDetail;
 using PostLand.Application.Features.Posts.Queries.GetPostList;
 using System;
 using System.Collections.Generic;
@@ -14,6 +16,8 @@ namespace PostLand.Application.Profiles
         public AutoMapperProfile() 
         {
             CreateMap<Post,GetPostListViewModel>().ReverseMap();
+            CreateMap<Post, GetPostDetailViewModel>().ReverseMap();
+            CreateMap<Post, CreatePostCommand>().ReverseMap();
         }
     }
 }
